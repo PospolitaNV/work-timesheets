5bfa91eb64e206ac7536bc0dedeacd2fc52b5ad1
NPospolita <nikita7asics@gmail.com>
2021-02-20 23:45:24 +0300

    more fucking annotations

--- src/main/kotlin/com/github/npospolita/worktimesheets/WorkTimesheetsApplication.kt
+++ src/main/kotlin/com/github/npospolita/worktimesheets/WorkTimesheetsApplication.kt
@@ -3,10 +3,12 @@ package com.github.npospolita.worktimesheets
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableWebMvc
 class WorkTimesheetsApplication
 
 fun main(args: Array<String>) {
