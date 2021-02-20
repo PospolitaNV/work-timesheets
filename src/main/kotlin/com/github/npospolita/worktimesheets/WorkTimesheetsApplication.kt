c884a25b9ad3fcab4cce8b7060f6b677f496bfcb
NPospolita <nikita7asics@gmail.com>
2021-02-20 23:29:44 +0300

    final?

--- src/main/kotlin/com/github/npospolita/worktimesheets/WorkTimesheetsApplication.kt
+++ src/main/kotlin/com/github/npospolita/worktimesheets/WorkTimesheetsApplication.kt
@@ -1,43 +1,15 @@
 package com.github.npospolita.worktimesheets
 
-import com.zaxxer.hikari.HikariDataSource
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.boot.SpringApplication
-import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
-import javax.sql.DataSource
 
 
 @SpringBootApplication
 @EnableJpaRepositories
-class WorkTimesheetsApplication {
-}
-
-@Configuration
-class DatabaseConfig {
-
-    @Bean
-    @Order(Int.MAX_VALUE)
-    fun dataSource(appContext: ApplicationContext): DataSource {
-        println()
-        println("HEROKU_POSTGRESQL_YELLOW_URL:" + appContext.environment.getProperty("HEROKU_POSTGRESQL_YELLOW_URL"))
-        println("JDBC_DATABASE_USERNAME:" + appContext.environment.getProperty("JDBC_DATABASE_USERNAME"))
-        println("JDBC_DATABASE_PASSWORD:" + appContext.environment.getProperty("JDBC_DATABASE_PASSWORD"))
-        println("SPRING_DATASOURCE_URL:" + appContext.environment.getProperty("SPRING_DATASOURCE_URL"))
-        println()
-        return HikariDataSource()
-    }
-
-}
+class WorkTimesheetsApplication
 
 fun main(args: Array<String>) {
-    println("--------------------------------")
-    println(System.getProperties())
-    println("--------------------------------")
     runApplication<WorkTimesheetsApplication>(*args)
 }
 
