ac176fd6db3799df325651f2f2c59d31f57c6cdc
NPospolita <nikita7asics@gmail.com>
2021-02-21 00:19:25 +0300

    test

--- src/main/kotlin/com/github/npospolita/worktimesheets/controller/WebhookReceiver.kt
+++ src/main/kotlin/com/github/npospolita/worktimesheets/controller/WebhookReceiver.kt
@@ -3,13 +3,14 @@ package com.github.npospolita.worktimesheets.controller
 import com.github.kotlintelegrambot.Bot
 import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class WebHookReceiver(
-    val bot: Bot
+//    val bot: Bot
 ) {
     companion object {
         val log = LoggerFactory.getLogger(WebHookReceiver::class.java.name)
@@ -19,7 +20,12 @@ class WebHookReceiver(
     @PostMapping(path = ["/\${bot.token}"], consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun receiveWebHook(@RequestBody request: String?) {
         log.info("Received update: {}", request)
-        bot.processUpdate(request!!)
+//        bot.processUpdate(request!!)
         log.info("Update parsed: {}", request)
+    }
+
+    @GetMapping("test")
+    fun test() {
+        log.error("hyi")
     }
 }
