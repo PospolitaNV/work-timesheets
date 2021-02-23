e549e21512f6796bf0e1b7dd94b886b07a27841b
NPospolita <nikita7asics@gmail.com>
2021-02-23 17:50:57 +0300

    Better codestyle

--- src/main/kotlin/com/github/npospolita/worktimesheets/bot/HandlerUtils.kt
+++ src/main/kotlin/com/github/npospolita/worktimesheets/bot/HandlerUtils.kt
@@ -3,13 +3,13 @@ package com.github.npospolita.worktimesheets.bot
 import com.github.kotlintelegrambot.entities.Update
 
 fun getChatId(update: Update): Long {
-    if (update.callbackQuery != null) return update.callbackQuery?.message?.chat?.id!!
-    if (update.message != null) return update.message?.chat?.id!!
-    throw Error("Can't extract chat id")
+    return update.callbackQuery?.message?.chat?.id
+        ?: update.message?.chat?.id
+        ?: throw Error("Can't extract chat id")
 }
 
 fun getUserId(update: Update): Long {
-    if (update.callbackQuery != null) return update.callbackQuery?.from?.id!!
-    if (update.message != null && update.message?.from != null) return update.message?.from?.id!!
-    throw Error("Can't extract user id")
+    return update.callbackQuery?.from?.id
+        ?: update.message?.from?.id
+        ?: throw Error("Can't extract user id")
 }
--- src/main/kotlin/com/github/npospolita/worktimesheets/bot/StartHandlers.kt
+++ src/main/kotlin/com/github/npospolita/worktimesheets/bot/StartHandlers.kt
@@ -4,58 +4,55 @@ import com.github.kotlintelegrambot.dispatcher.handlers.HandleUpdate
 import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
 
-class StartHandlers {
-    companion object {
+object StartHandlers {
 
-        fun admin(): HandleUpdate {
-            return { bot, update ->
-                bot.sendMessage(
-                    getChatId(update), "Выберите операцию",
-                    replyMarkup = adminStartKeyboardMarkup()
-                )
-            }
-        }
-
-        fun employee(): HandleUpdate {
-            return { bot, update ->
-                bot.sendMessage(
-                    getChatId(update), "Выберите операцию",
-                    replyMarkup = employeeStartKeyboardMarkup()
-                )
-            }
-        }
-
-        fun employeeStartKeyboardMarkup() = InlineKeyboardMarkup.create(
-            listOf(
-                InlineKeyboardButton.CallbackData(
-                    text = "Отметить приход",
-                    callbackData = "check_in"
-                ),
-                InlineKeyboardButton.CallbackData(
-                    text = "Отметить уход",
-                    callbackData = "check_out"
-                )
-            ),
-            listOf(
-                InlineKeyboardButton.CallbackData(
-                    text = "Посмотреть статистику",
-                    callbackData = "employee_stats"
-                ),
+    fun admin(): HandleUpdate {
+        return { bot, update ->
+            bot.sendMessage(
+                getChatId(update), "Выберите операцию",
+                replyMarkup = adminStartKeyboardMarkup()
             )
-        )
-
-        fun adminStartKeyboardMarkup() = InlineKeyboardMarkup.create(
-            listOf(
-                InlineKeyboardButton.CallbackData(
-                    text = "Сотрудники",
-                    callbackData = "employee_list"
-                ),
-                InlineKeyboardButton.CallbackData(
-                    text = "Расчёт зарплаты",
-                    callbackData = "salary_options"
-                )
-            )
-        )
-
+        }
     }
+
+    fun employee(): HandleUpdate {
+        return { bot, update ->
+            bot.sendMessage(
+                getChatId(update), "Выберите операцию",
+                replyMarkup = employeeStartKeyboardMarkup()
+            )
+        }
+    }
+
+    fun employeeStartKeyboardMarkup() = InlineKeyboardMarkup.create(
+        listOf(
+            InlineKeyboardButton.CallbackData(
+                text = "Отметить приход",
+                callbackData = "check_in"
+            ),
+            InlineKeyboardButton.CallbackData(
+                text = "Отметить уход",
+                callbackData = "check_out"
+            )
+        ),
+        listOf(
+            InlineKeyboardButton.CallbackData(
+                text = "Посмотреть статистику",
+                callbackData = "employee_stats"
+            ),
+        )
+    )
+
+    fun adminStartKeyboardMarkup() = InlineKeyboardMarkup.create(
+        listOf(
+            InlineKeyboardButton.CallbackData(
+                text = "Сотрудники",
+                callbackData = "employee_list"
+            ),
+            InlineKeyboardButton.CallbackData(
+                text = "Расчёт зарплаты",
+                callbackData = "salary_options"
+            )
+        )
+    )
 }
